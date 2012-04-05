1161c444e5911deca6c1d5cd698df1d1d2e46a7f
Jordan Miron <CalamariDamacy@gmail.com>
2012-04-05 18:24:57 -0400

    main file

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+from pygame.sprite import Group, GroupSingle, groupcollide
+
+from player import Player
+
+SCREEN_SIZE = 640,480
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    bounds = screen.get_rect
+
+
+    player =
+    level = 
